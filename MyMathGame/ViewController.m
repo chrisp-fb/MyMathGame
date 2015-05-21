7dd5d34fcab44e91f40b5e509a89c06cfc6ede39
chris pan <chrisp@fb.com>
2015-05-21 11:59:52 -0700

    Implement game logic

--- MyMathGame/ViewController.m
+++ MyMathGame/ViewController.m
@@ -8,20 +8,84 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+  NSInteger _currentLevel;
+}
+
+@property (nonatomic, assign) NSInteger currentLevel;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+  _currentLevel = [[NSUserDefaults standardUserDefaults] integerForKey:@"currentLevel"];
 }
 
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+- (void)setCurrentLevel:(NSInteger)currentLevel
+{
+  _currentLevel = currentLevel;
+  [[NSUserDefaults standardUserDefaults] setInteger:_currentLevel forKey:@"currentLevel"];
+}
+
+- (NSInteger)currentLevel
+{
+  return _currentLevel;
+}
+
+#pragma mark UITableViewDelegate
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"levelCell"];
+  UILabel *label = (UILabel *)[cell viewWithTag:7];
+  label.text = [NSString stringWithFormat:@"Level %d", (int) indexPath.row +1];
+  if (indexPath.row < self.currentLevel) {
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.userInteractionEnabled = NO;
+  } else {
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.selectionStyle = UITableViewCellSelectionStyleGray;
+    cell.userInteractionEnabled = YES;
+  }
+  return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  NSString *title = [NSString stringWithFormat:@"Level %d", (int) self.currentLevel+1];
+  int r = arc4random_uniform((uint) (10 * self.currentLevel));
+  int r2 = arc4random_uniform((uint) (10 * self.currentLevel));
+  int answer = r + r2;
+  NSString *message = [NSString stringWithFormat:@"What is %d + %d ?", r, r2];
+  UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+  [alertController addTextFieldWithConfigurationHandler:NULL];
+  UIAlertAction *action = [UIAlertAction actionWithTitle:@"Continue" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+    int userAnswer = (int) [((UITextField*)alertController.textFields[0]).text integerValue];
+    if (userAnswer == answer) {
+      self.currentLevel = self.currentLevel + 1;
+      [self.tableView reloadData];
+    }
+  }];
+  [alertController addAction:action];
+  [self presentViewController:alertController animated:YES completion:NULL];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+  return self.currentLevel + 1;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+  return 1;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+  return @"Levels";
 }
 
 @end
